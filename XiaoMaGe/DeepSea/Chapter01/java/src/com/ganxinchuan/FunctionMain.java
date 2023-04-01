352fc7ce25e77666d99ec645a5baffb8541ae62a
“xegbertgxcgit config --global user.email “xxx”1415051501@qq.com <1415051501@qq.com>
2023-04-01 09:20:41 +0800

    update

---
+++ XiaoMaGe/DeepSea/Chapter01/java/src/com/ganxinchuan/FunctionMain.java
@@ -0,0 +1,18 @@
+package com.ganxinchuan;
+
+public class FunctionMain {
+
+    /**
+     * java  9之前的
+     * public all
+     * protected 继承+同包
+     * (default) 同包
+     * private 当前类
+     *
+     * java9之后
+     * @param args
+     */
+    public static void main(String[] args) {
+        //byte(1) char(2)   short(2) int(4) long(8) float(4) double(8) boolean(1)
+    }
+}
